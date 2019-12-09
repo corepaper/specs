8f5db8d95e7b0b95b8dccd1595bb7c5bdcc84b75
Wei Tang <hi@that.world>
2019-12-09 17:51:57 +0100

    Add vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "Core Paper",
+  description: "Specification repository for Core Paper.",
+  themeConfig: {
+    repo: 'corepaper/specs',
+    editLinks: true,
+    smoothScroll: true,
+    navbar: false,
+    sidebar: false,
+  },
+  plugins: [
+    [
+      'vuepress-plugin-clean-urls',
+      {
+        normalSuffix: '/',
+        indexSuffix: '/',
+        notFoundPath: '/404.html',
+      },
+    ],
+  ],
+};
